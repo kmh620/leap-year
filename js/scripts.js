ea36910b72b7b0f690f38bc2276e8d4189bc1947
Katrina Hockman and Noah Schooler <noah@>
2018-10-24 09:52:27 -0700

    separate business and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
+var leapYear = function(year) {
+  if (year % 100 === 0 && year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 $(document).ready(function(){
   $("#leap-year").submit(function(){
     event.preventDefault();
     var year = $("#year").val();
+    var result = leapYear(year);
 
-    if (year % 100 === 0 && year % 400 === 0) {
-      $("#results").text("true")
-    } else if (year % 100 === 0) {
-      $("#results").text("false")
-    } else if (year % 4 === 0) {
-      $("#results").text("true")
+    if (!result) {
+      $("#results").text("Not a leap year");
     } else {
-      $("#results").text("false")
+      $("#results").text("It's a leap year!");
     }
-
   });
 });
